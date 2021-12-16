6b28b0a8b4d5d40bb3cdc650c5a82f11f77e23fe
joshgit <jschermann91@gmail.com>
2021-12-16 17:41:12 -0600

    delete and post

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Express Notes Lover!");
+});
+
+app.post("/", function (req, res) {
+  res.send("Got a POST request!");
+});
+
+app.delete("/", function (req, res) {
+  res.send("Got a DELETE request at /user");
 });
 
 app.listen(port, () => {
