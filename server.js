2b17e0d19096c2c2c6d5e7eb3832477679acdc0b
joshgit <jschermann91@gmail.com>
2021-12-16 17:49:46 -0600

    notes redirect

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+// const { getNotes } = require();
+
+// This allows public directory to be served
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello Express Notes Lover!");
+});
+
+app.get("/notes", (req, res) => {
+  res.send("Notes?!");
+  //   getNotes();
 });
 
 app.post("/", function (req, res) {
